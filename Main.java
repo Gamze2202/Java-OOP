47d012dabe73c6129ad82f5754263eb656af0c31
Gamze <113441133+Gamze2202@users.noreply.github.com>
2025-06-08 17:13:31 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Student p = new Student();
+        p.setName("Gamze");
+        p.setId(2373496);
+        p.setGrade(100);
+        System.out.println(p.getName());
+        System.out.println(p.getId());
+        System.out.println(p.getGrade());
+
+
+    }
+    
+    
+}
---
+++ Student.java
@@ -0,0 +1,29 @@
+public class Student {
+    private String name;
+
+    public void setName(String newName) {
+        name = newName;
+    }
+    private int id;
+    
+    public void setId(int newId) {
+        id = newId;
+    }
+    private double grade;
+    public void setGrade(double newGrade) {
+        grade = newGrade;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public double getGrade() {
+        return grade;
+    }
+    
+}
